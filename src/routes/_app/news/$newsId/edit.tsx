b921405fd42455d8b5272083782b77704541d8e6
vivekguptaqwerty <vivekg3216@gmail.com>
2026-07-25 19:10:52 +0530

    fixed issues

--- src/routes/_app/news/$newsId/edit.tsx
+++ src/routes/_app/news/$newsId/edit.tsx
@@ -47,7 +47,6 @@ const schema = z.object({
   districtId: z.string().optional(),
   areaId: z.string().optional(),
   visibilityScope: z.enum(["all_india", "state", "district", "area", "private"]),
-<<<<<<< HEAD
   visibilityStateIds: z.array(z.string()).default([]),
   visibilityDistrictIds: z.array(z.string()).default([]),
   visibilityAreaIds: z.array(z.string()).default([]),
@@ -85,18 +84,6 @@ const schema = z.object({
       ctx.addIssue({ code: z.ZodIssueCode.custom, path: ["pollOptions"], message: "Add at least two poll options" });
     }
   }
-=======
-  visibilityStateIds: z.array(z.string()),
-  visibilityDistrictIds: z.array(z.string()),
-  visibilityAreaIds: z.array(z.string()),
-  visibilityUserIds: z.array(z.string()),
-  sourceLink: z.string().url().or(z.literal("")).optional(),
-  thumbnailUrl: z.string().url().or(z.literal("")).optional(),
-  tagsText: z.string().optional(),
-  enablePoll: z.boolean(),
-  pollQuestion: z.string().optional(),
-  pollOptions: z.array(z.string()),
->>>>>>> 49b00521fc221b60fba67cd66393986f29286f41
 });
 
 type FormValues = z.infer<typeof schema>;
